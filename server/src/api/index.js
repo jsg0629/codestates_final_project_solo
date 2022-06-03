e85fe0d0b8e01de0ae761758a79c71052a3db661
jsg0629 <tnrms513@gmail.com>
2022-06-04 01:57:58 +0900

    docs: [server] routes, services 폴더추가 api 작업시작

--- server/src/api/index.js
+++ server/src/api/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
+import main from './routes/main'
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send('🚀 API Server is running...')
 })
+
+router.use('/main', main)
 
 export default router
---
+++ server/src/api/routes/main.js
@@ -0,0 +1,4 @@
+import { Router } from 'express'
+import MainService from '../../services/main'
+
+const router = Router()
