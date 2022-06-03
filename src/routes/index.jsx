d9211fe98cb4a3b6a15fe9eb1bc90e20fb414632
jsg0629 <tnrms513@gmail.com>
2022-06-03 22:31:02 +0900

    design: footer markup, design

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import styles from './Routes.module.scss'
+import { FaGithub } from 'react-icons/fa'
+import { SiNotion } from 'react-icons/si'
 
 import ArtDetail from './ArtDetail'
 import Discover from './Discover'
@@ -29,6 +31,28 @@ const App = () => {
             <Route path='*' element={<div>404</div>} />
           </Routes>
         </main>
+        <footer>
+          <div className={styles.footerWrapper}>
+            <div className={styles.firstItem}>©2022 Art1st. All Rights Reserved.</div>
+            <div className={styles.secondItem}>
+              <a
+                href='https://github.com/jsg0629/wanted_final_project'
+                target='_blank'
+                style={{ marginRight: '30px' }}
+                rel='noreferrer'
+              >
+                <FaGithub className={styles.githubIcon} size={25} />
+              </a>
+              <a
+                href='https://fluoridated-cayenne-db1.notion.site/Wanted_final_project_-3770a1b65d3244f18f7b3b56579ac2c5'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <SiNotion className={styles.notionIcon} size={25} />
+              </a>
+            </div>
+          </div>
+        </footer>
       </div>
     </div>
   )
