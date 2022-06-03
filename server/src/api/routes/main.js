527dc90b14892b9f77a22adae7c3d65f899f41b6
jsg0629 <tnrms513@gmail.com>
2022-06-04 02:08:54 +0900

    feat: [server] main page api 로직구현

--- server/src/api/routes/main.js
+++ server/src/api/routes/main.js
@@ -2,3 +2,16 @@ import { Router } from 'express'
 import MainService from '../../services/main'
 
 const router = Router()
+
+const MainServiceInstance = new MainService()
+
+router.get('/', async (req, res) => {
+  try {
+    const data = await MainServiceInstance.getMain()
+    return res.status(200).json(data)
+  } catch (err) {
+    return res.status(404).json(err.toString())
+  }
+})
+
+export default router
---
+++ server/src/services/main.js
@@ -0,0 +1,28 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-await-in-loop */
+import db from '../models/index'
+
+class MainService {
+  constructor() {
+    this.User = db.User
+    this.Profile = db.Profile
+    this.Artwork = db.Artwork
+  }
+
+  async getMain() {
+    try {
+      const banner = await db.Artwork.findOne({
+        // 가장 view가 많은 작품을 가져옴.
+        attributes: ['id', 'imgURI'],
+        order: [['views', 'DESC']],
+        limit: 1,
+      })
+
+      return banner // artworks가 하나도 없으면 빈 배열 반환
+    } catch (err) {
+      throw Error(err.toString())
+    }
+  }
+}
+
+export default MainService
