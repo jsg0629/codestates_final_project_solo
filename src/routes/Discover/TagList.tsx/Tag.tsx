c453c428c0007c46c02cc0a28d1bdb3961a446f8
jsg0629 <tnrms513@gmail.com>
2022-06-04 19:24:20 +0900

    feat: Discover페이지 TagList에서 Tag 클릭 시 버튼색상 변하는 기능추가

--- src/routes/Discover/TagList.tsx/Tag.tsx
+++ src/routes/Discover/TagList.tsx/Tag.tsx
@@ -1,10 +1,26 @@
 import styles from './TagList.module.scss'
 import { ITopTagData } from 'types/artwork'
 
-const Tag = ({ tagData }: { tagData: ITopTagData }) => {
+const Tag = ({
+  tagData,
+  clickedTagId,
+  handleTagClick,
+}: {
+  tagData: ITopTagData
+  clickedTagId: number
+  handleTagClick: Function
+}) => {
   return (
     <li>
-      <button type='button'>{tagData.hashtag}</button>
+      <button
+        type='button'
+        onClick={() => {
+          handleTagClick(tagData.id)
+        }}
+        className={clickedTagId === tagData.id ? styles.fill : ''}
+      >
+        {tagData.hashtag}
+      </button>
     </li>
   )
 }
--- src/routes/Discover/TagList.tsx/index.tsx
+++ src/routes/Discover/TagList.tsx/index.tsx
@@ -2,20 +2,28 @@ import styles from './TagList.module.scss'
 import Tag from './Tag'
 
 import { useRecoil } from 'hooks/state'
+import { useState } from 'react'
 import { topTagDataState } from 'states/artwork'
 
 import { ITopTagData } from 'types/artwork'
 
 const TagList = () => {
   const [tagsData] = useRecoil(topTagDataState)
+  const [clickedTagId, setClickedTagId] = useState(0)
   console.log(tagsData)
+
+  const handleTagClick = (Id: number) => {
+    setClickedTagId(Id)
+  }
   return (
     <ul className={styles.tagListWrapper}>
       <li>
-        <button type='button'>전체</button>
+        <button type='button' onClick={() => handleTagClick(0)} className={clickedTagId === 0 ? styles.fill : ''}>
+          전체
+        </button>
       </li>
       {tagsData.map((tagData: ITopTagData) => {
-        return <Tag tagData={tagData} key={tagData.id} />
+        return <Tag tagData={tagData} key={tagData.id} clickedTagId={clickedTagId} handleTagClick={handleTagClick} />
       })}
     </ul>
   )
