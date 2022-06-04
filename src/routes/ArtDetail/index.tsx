b9121780ecb31609e12fdc5191f4dacf998b8b68
jsg0629 <tnrms513@gmail.com>
2022-06-05 02:47:46 +0900

    feat: ArtDetail 페이지 axios 로직구현 getArtworkById

--- src/routes/ArtDetail/index.tsx
+++ src/routes/ArtDetail/index.tsx
@@ -1,11 +1,16 @@
 /* eslint-disable camelcase */
 import styles from './ArtDetail.module.scss'
+import { useQuery } from 'react-query'
+import { getArtworkById } from 'services/artwork/get'
 
 import { useParams } from 'react-router-dom'
 
 const ArtDetail = () => {
   const { artwork_id } = useParams()
-  console.log(artwork_id)
+
+  const { data: artworkData, isError, isLoading } = useQuery(['artwork', artwork_id], getArtworkById(artwork_id))
+
+  console.log(artworkData)
   return <div>ArtDetail</div>
 }
 
--- src/services/artwork/get.ts
+++ src/services/artwork/get.ts
@@ -1,15 +1,24 @@
+/* eslint-disable camelcase */
 import { HOST_ADDRESS } from 'utils/constant/index'
 import axios from 'axios'
 
+// 가장 많이 사용한 상위태그 10개
 export const getMostUsedTags = (setTopTagData: Function) => {
   return axios.get(`${HOST_ADDRESS}/artwork/tag/top`).then((res) => {
     return setTopTagData(res.data)
   })
 }
 
+// 모든 artwork info
 export const getArtworkList = (tagId?: number) => async () => {
   const { data } = await axios.get(`${HOST_ADDRESS}/artwork`, {
     params: { tag_id: tagId },
   })
+  return data
+}
+
+// 특정 artwork info
+export const getArtworkById = (artwork_id: string | string[] | undefined) => async () => {
+  const { data } = await axios.get(`${HOST_ADDRESS}/artwork/${artwork_id}`)
   return data
 }
