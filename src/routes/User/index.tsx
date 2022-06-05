0a30e0f1c1b861eca9311aeb7e374a2ddeb98ebc
jsg0629 <tnrms513@gmail.com>
2022-06-05 15:46:13 +0900

    design: user page  markup design

--- src/routes/User/index.tsx
+++ src/routes/User/index.tsx
@@ -5,18 +5,36 @@ import { useQuery } from 'react-query'
 
 import { getUserById } from 'services/user/get'
 
+import Loading from 'components/Loading'
+import { AiOutlineInstagram, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai'
+
 const User = () => {
   const { user_id } = useParams()
 
-  const {
-    data: userData,
-    isError: userIsError,
-    isLoading: userIsLoading,
-  } = useQuery(['user', user_id], getUserById(user_id))
+  const { data: userData, isError: userIsError, isLoading } = useQuery(['user', user_id], getUserById(user_id))
 
-  console.log(userData)
-
-  return <div>User</div>
+  return (
+    <div className={styles.userWrapper}>
+      {isLoading ? (
+        <Loading heightValue={undefined} />
+      ) : (
+        <div className={styles.userContentsWrapper}>
+          <div className={styles.userImgBox}>
+            <img src={userData.user_profile.picture} alt='profile img' />
+          </div>
+          <div className={styles.userName}>{userData.user.name}</div>
+          <div className={styles.userMoreInform}>
+            <AiOutlineInstagram size={25} />
+            <AiOutlineTwitter size={25} />
+            <AiFillFacebook size={25} />
+          </div>
+          <button type='button' className={styles.donateBtn}>
+            Donate
+          </button>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default User
